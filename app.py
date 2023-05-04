5e0a556a8c592f6a50b08a02f933ccd5fbb1a54f
Oscar Larsen <oscarpopp98@gmail.com>
2023-05-04 12:24:43 +0200

    added minor changes

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.title("Hello World")
+
+st.write("This is a simple example of Streamlit")
+
+st.session_state.color = st.radio("What is your favorite color?", ("Blue", "Red", "Green"))
+
+if st.session_state.color != "":
+    st.write(f"Your favorite color is {st.session_state.color}")
