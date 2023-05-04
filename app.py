a2694d60f96671da0b3908b8ccacb7668c4fe394
Oscar Larsen <oscarpopp98@gmail.com>
2023-05-04 16:02:48 +0200

    added config file

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import numpy as np
+
+
+if "color" not in st.session_state:
+    st.session_state.color = "Green"
+
 
 st.title("Hello World")
 
@@ -8,3 +14,10 @@ st.session_state.color = st.radio("What is your favorite color?", ("Blue", "Red"
 
 if st.session_state.color != "":
     st.write(f"Your favorite color is {st.session_state.color}")
+
+
+
+with st.container():
+    st.write("This is inside the container")
+    st.button("This is a button")
+    st.bar_chart(np.random.randn(50, 3))
